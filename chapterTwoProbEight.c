e6083934f32334e7706828eee710dbb44f53ba63
Drew Barranco <dgbarrancolanda@dons.usfca.edu>
2026-06-29 20:23:20 -0700

    Fix variable typos and improve formatting

--- chapterTwoProbEight.c
+++ chapterTwoProbEight.c
@@ -1,38 +1,39 @@
 #include <stdio.h>
 
-int main(void){
-  flaot loan, interextRate, monthlyPayment;
-  float monthlyInterestRate;
-  float balance;
+int main(void) {
+    float loan, interestRate, monthlyPayment;
+    float monthlyInterestRate;
+    float balance;
+    int month = 1;
 
+    printf("Enter amount of loan: ");
+    scanf("%f", &loan);
 
-  printf("Enter amount of loan: ");
-  scanf("%f", &loan);
+    printf("Enter interest rate: ");
+    scanf("%f", &interestRate);
 
-  prinf("Enter interest rate: ");
-  scanf("%f", &interestRate);
+    printf("Enter monthly payment: ");
+    scanf("%f", &monthlyPayment);
 
-  printf("Enter monthly payment");
-  scanf("%f", &monthlyPayment);
+    monthlyInterestRate = (interestRate / 100) / 12;
 
-  montlyInterestRate = (interestRate/100)/12;
+    balance = loan;
 
-  balance = loan;
+    while (month <= 3) {
+        balance = balance * (1 + monthlyInterestRate) - monthlyPayment;
 
+        if (month == 1) {
+            printf("Balance remaining after first payment: $%.2f\n", balance);
+        }
+        else if (month == 2) {
+            printf("Balance remaining after second payment: $%.2f\n", balance);
+        }
+        else {
+            printf("Balance remaining after third payment: $%.2f\n", balance);
+        }
 
-  while(month <= 3){
-    balance = balance * (1+monthlyInterestRate) - monthlyPayment;
-    if(month ==1){
-      printf("Balance remaining after first payment: $%.2f\n", balance);
-}
-    else if(month ==2){
-      printf("Balance remaining after second payment: $%.2f\n", balance);
-}
-    else{
-      printf("Balance remaining after third payment: $%.2f\n", balance);
-}
+        month++;
+    }
 
-    month++;
-  }
-  return 0;
+    return 0;
 }
