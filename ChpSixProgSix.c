1a8e3bdb7bf2d9c42088540bd4f8a6899e095064
Drew Barranco <dgbarrancolanda@dons.usfca.edu>
2026-07-06 20:24:59 -0700

    Add program to print squares of even numbers

---
+++ ChpSixProgSix.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    int i;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for (i = 2; i * i <= n; i += 2)
+    {
+        printf("%d\n", i * i);
+    }
+
+    return 0;
+}
