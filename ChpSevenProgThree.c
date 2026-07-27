00215a0764945eb9f79eacf6d3dab2680a008877
Drew Barranco <dgbarrancolanda@dons.usfca.edu>
2026-07-27 16:55:12 -0700

    Add program to sum a series of doubles

---
+++ ChpSevenProgThree.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    double n, sum = 0.0;
+
+    printf("This program sums a series of doubles.\n");
+    printf("Enter doubles (0 to stop): ");
+
+    scanf("%lf", &n);
+
+    while (n != 0) {
+        sum += n;
+        scanf("%lf", &n);
+    }
+
+    printf("The sum is %.2f\n", sum);
+
+    return 0;
+}
