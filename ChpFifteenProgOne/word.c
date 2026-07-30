046537a3a55c2f0caa270674a837db015a10ceb2
Drew Barranco <dgbarrancolanda@dons.usfca.edu>
2026-07-29 22:57:17 -0700

    Refactor read_word function for improved handling
    Refactor read_word to handle leading spaces and truncation.

--- ChpFifteenProgOne/word.c
+++ ChpFifteenProgOne/word.c
@@ -3,23 +3,46 @@
 
 int read_char(void)
 {
-  int ch = getchar();
-  if(ch == '\n' || ch == '\t'){
-    return ' ';
-  }
-  return ch;
+    int ch = getchar();
+
+    if (ch == '\n' || ch == '\t')
+        return ' ';
+
+    return ch;
 }
 
-void reach_word(char *word, int len)
+void read_word(char *word, int len)
 {
-  int ch, pos = 0;
-  while((ch = read_char()) == ' ')
-    ;
-  while(ch != ' ' && ch != EOF) {
-    if(pos < len){
-      word[pos++] = ch;
+    int ch, pos = 0;
+    int truncated = 0;
+
+    /* Skip leading spaces */
+    while ((ch = read_char()) == ' ')
+        ;
+
+    /* Read the word */
+    while (ch != ' ' && ch != EOF)
+    {
+        if (pos < len - 1)
+        {
+            word[pos++] = ch;
+        }
+        else
+        {
+            truncated = 1;
+        }
+
+        ch = read_char();
     }
-    ch = read_char();
-  }
-  word[pos] = '/0';
+
+    /* Add * if the word was truncated */
+    if (truncated && len > 1)
+    {
+        word[len - 2] = '*';
+        word[len - 1] = '\0';
+    }
+    else
+    {
+        word[pos] = '\0';
+    }
 }
