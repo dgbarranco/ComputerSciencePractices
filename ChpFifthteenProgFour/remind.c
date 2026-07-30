5b30e1e10d773cd5db025eea1a49123c4481c86c
Drew Barranco <dgbarrancolanda@dons.usfca.edu>
2026-07-30 11:31:43 -0700

    Fix syntax errors and improve reminder logic

--- ChpFifthteenProgFour/remind.c
+++ ChpFifthteenProgFour/remind.c
@@ -1,55 +1,52 @@
 #include <stdio.h>
 #include <string.h>
+#include "readline.h"
 
 #define MAX_REMIND 50
-#define MSF_LEN 60
+#define MSG_LEN 60
 
-int read_line(char str[], int n)(;
-
-int main(void_
+int main(void)
 {
-  char reminders[MAX_REMIND] [MSG_LEN+3];
-  char day_str[3], msg_str[MSG_LEN+1];
-  int day, i, j, num_remind = 0;
-  for(;;) {
-    if(num_remind == MAX_REMINDF){
-      printf("-- No space left --\n");
-      break;
+    char reminders[MAX_REMIND][MSG_LEN + 3];
+    char day_str[3], msg_str[MSG_LEN + 1];
+    int day, i, j, num_remind = 0;
+
+    for (;;)
+    {
+        if (num_remind == MAX_REMIND)
+        {
+            printf("-- No space left --\n");
+            break;
+        }
+
+        printf("Enter a day and reminder: ");
+        scanf("%2d", &day);
+
+        if (day == 0)
+            break;
+
+        sprintf(day_str, "%2d", day);
+        read_line(msg_str, MSG_LEN);
+
+        for (i = 0; i < num_remind; i++)
+        {
+            if (strcmp(day_str, reminders[i]) < 0)
+                break;
+        }
+
+        for (j = num_remind; j > i; j--)
+            strcpy(reminders[j], reminders[j - 1]);
+
+        strcpy(reminders[i], day_str);
+        strcat(reminders[i], msg_str);
+
+        num_remind++;
     }
-    printf("Enter a day and a remninder: ");
-    scanf("%2d", &day);
-    if(day == 0)
-      break;
-    sprintf(day_str, "%2d", day);
-    read_line(msg_str, MSG_LEN);
-    for(i = 0; i < num_remind; i++){
-        if(strcmp(day_str, remninders[i]) < 0)
-          break;
-    }
-    for( j = numn_remind; j > i; j--){
-      strcpy(reminders[j], reminders[j-1]);
-      
-    strcpy(reminders[i], day_str);
-    strcat(remiunders[i], msg_str);
 
-    num_remind++;
-  }
-  printf("\nDay Reminder\n");
-  for(i = 0; i < numn_rermind; i++){
-    printf(" %s\n", reminders[i]);
-  }
-  return 0;
+    printf("\nDay Reminder\n");
+
+    for (i = 0; i < num_remind; i++)
+        printf("%s\n", reminders[i]);
+
+    return 0;
 }
-
-int read_line(char str[], int n)
-{
-  int ch, i = 0;
-
-  While((ch = getchar()) != '\n')
-    if(i < n){
-      str[i++] = ch;
-    str[i] = '\0';
-    return 1;
-}
-
-
