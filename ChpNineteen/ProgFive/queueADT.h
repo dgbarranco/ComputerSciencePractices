dd56c5643f464ec9169910091e28839e1200b79d
Drew Barranco <dgbarrancolanda@dons.usfca.edu>
2026-08-04 18:48:02 -0700

    Add header file for queue ADT implementation

---
+++ ChpNineteen/ProgFive/queueADT.h
@@ -0,0 +1,22 @@
+#ifndef QUEUEADT_H
+#define QUEUEADT_H
+
+#include <stdbool.h>
+
+typedef struct queue_type *Queue;
+
+Queue create(void);
+void destroy(Queue q);
+
+void make_empty(Queue q);
+
+bool is_empty(Queue q);
+bool is_full(Queue q);
+
+void insert(Queue q, int item);
+int remove_item(Queue q);
+
+int first(Queue q);
+int last(Queue q);
+
+#endif
