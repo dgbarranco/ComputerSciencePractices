14f1fc0ff669374315621681302f5370f3b1f6fc
Drew Barranco <dgbarrancolanda@dons.usfca.edu>
2026-08-04 22:09:18 -0700

    Add union and struct for float representation

--- ChpTwenty/ProgOne/main.c
+++ ChpTwenty/ProgOne/main.c
@@ -1,1 +1,26 @@
 
+#include <stdio.h>
+
+struct float_bits {
+    unsigned int fraction : 23;   // bits 0-22
+    unsigned int exponent : 8;    // bits 23-30
+    unsigned int sign : 1;        // bit 31
+};
+
+union float_view {
+    float value;
+    struct float_bits bits;
+};
+
+int main(void)
+{
+    union float_view u;
+
+    u.bits.sign = 1;
+    u.bits.exponent = 128;
+    u.bits.fraction = 0;
+
+    printf("Float value: %f\n", u.value);
+
+    return 0;
+}
