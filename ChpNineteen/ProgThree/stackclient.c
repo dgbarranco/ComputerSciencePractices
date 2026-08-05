3d1cff68949d30c4f4d380462cfc24cf39737f5e
Drew Barranco <dgbarrancolanda@dons.usfca.edu>
2026-08-04 18:24:43 -0700

    Fix syntax errors and improve stack client logic
    Refactor stack client code for clarity and fix errors.

--- ChpNineteen/ProgThree/stackclient.c
+++ ChpNineteen/ProgThree/stackclient.c
@@ -1,38 +1,54 @@
 #include <stdio.h>
 #include "stackADT.h"
 
-in t main(voidf)
+int main(void)
 {
-Stack s1, s2;
-int n;
+    Stack s1, s2;
+    int n;
 
-s1 = create();
-s2 = create();
+    s1 = create();
+    s2 = create();
 
-push(s1, 1);
-push(s1, 2);
+    push(s1, 1);
+    printf("Length of s1 = %d\n", length(s1));
 
-n = pop();
-printf("Popped %d from s1\n", n);
-push(s2, n);
-n = pop(s1);
-printf("Popped %d from s1\n", n);
-push(s2, n);
+    push(s1, 2);
+    printf("Length of s1 = %d\n", length(s1));
 
-derstroy(s1);
+    n = pop(s1);
+    printf("Popped %d from s1\n", n);
+    printf("Length of s1 = %d\n", length(s1));
 
-while(!is_empty(s2))
-printf("Popped %d from s2\n", pop(s2));
+    push(s2, n);
+    printf("Length of s2 = %d\n", length(s2));
 
-push(s2, 3);
+    n = pop(s1);
+    printf("Popped %d from s1\n", n);
+    printf("Length of s1 = %d\n", length(s1));
 
-make_empty(s2);
+    push(s2, n);
+    printf("Length of s2 = %d\n", length(s2));
 
-if(is_empotry(s2))
-printf("s2 isd emptty\n");
-else
-printf("s2 is not empty\n");
-destory(s2);
-return 0;
+    destroy(s1);
 
+    while (!is_empty(s2))
+    {
+        printf("Popped %d from s2\n", pop(s2));
+        printf("Length of s2 = %d\n", length(s2));
+    }
+
+    push(s2, 3);
+    printf("Length of s2 = %d\n", length(s2));
+
+    make_empty(s2);
+    printf("Length of s2 = %d\n", length(s2));
+
+    if (is_empty(s2))
+        printf("s2 is empty\n");
+    else
+        printf("s2 is not empty\n");
+
+    destroy(s2);
+
+    return 0;
 }
