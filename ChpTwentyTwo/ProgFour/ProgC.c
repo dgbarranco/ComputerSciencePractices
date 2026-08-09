f4665c0f117d7a117bdee4f4612645f9296ac2a9
Drew Barranco <dgbarrancolanda@dons.usfca.edu>
2026-08-08 20:06:12 -0700

    Add line counting program in ProgC.c
    This program counts the number of lines in a given file and prints the result.

---
+++ ChpTwentyTwo/ProgFour/ProgC.c
@@ -0,0 +1,33 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  FILE *fp;
+  int ch;
+  int count = 0;
+
+  if (argc != 2) {
+    printf("usage: countlines filename\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if ((fp = fopen(argv[1], "r")) == NULL) {
+    printf("can't open %s\n", argv[1]);
+    exit(EXIT_FAILURE);
+  }
+
+  while ((ch = fgetc(fp)) != EOF) {
+    if (ch == '\n') {
+      count++;
+    }
+  }
+
+  printf("Number of lines: %d\n", count);
+
+  fclose(fp);
+
+  return 0;
+}
+
