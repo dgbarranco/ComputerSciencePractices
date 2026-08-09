23db7a05a2e6364b6f6167c0fa2d4378f2d6e798
Drew Barranco <dgbarrancolanda@dons.usfca.edu>
2026-08-08 19:42:43 -0700

    Add canopen.c to check file accessibility

---
+++ ChpTwentyTwo/ProgOne/canopen.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  FILE *fp;
+  int i;
+  int failed = 0;
+
+  if (argc < 2) {
+    printf("usage: canopen filename...\n");
+    exit(EXIT_FAILURE);
+  }
+
+  for (i = 1; i < argc; i++) {
+    if ((fp = fopen(argv[i], "r")) != NULL) {
+      printf("%s can be opened\n", argv[i]);
+      fclose(fp);
+    } else {
+      printf("%s can't be opened\n", argv[i]);
+      failed = 1;
+    }
+  }
+
+  if (failed)
+    return EXIT_FAILURE;
+
+  return EXIT_SUCCESS;
+}
