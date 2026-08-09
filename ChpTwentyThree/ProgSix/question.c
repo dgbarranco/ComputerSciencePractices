6c564b6fef4006c99cd95de6322b73d7a6cfb392
Drew Barranco <dgbarrancolanda@dons.usfca.edu>
2026-08-08 20:29:14 -0700

    Add program to replace control characters with '?'

---
+++ ChpTwentyThree/ProgSix/question.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+  int ch;
+
+  while ((ch = getchar()) != EOF) {
+    if (iscntrl(ch) && ch != '\n')
+      ch = '?';
+
+    putchar(ch);
+  }
+
+  return 0;
+}
+
